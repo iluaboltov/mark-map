6a921d6d0efb8080b449601baa9e2af269e1baf8
iluaboltov <iluaboltov@gmail.com>
2024-03-29 11:54:12 -0400

    WP

--- src/components/marker-card/marker-menu.tsx
+++ src/components/marker-card/marker-menu.tsx
@@ -1,4 +1,6 @@
-const MarkerMenu = ({children}) => {
+import { ReactNode } from "react";
+
+const MarkerMenu = ({children}: {children: ReactNode}) => {
   return (
     <div className={'flex flex-col absolute right-1 top-1/2 -translate-y-1/2'}>
       {children}
