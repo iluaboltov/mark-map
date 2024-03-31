27a9f173eef744738a651f7c31f7dbd03a50c03a
iluaboltov <iluaboltov@gmail.com>
2024-03-30 23:45:35 -0400

    WP

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -8,14 +8,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PRIVATE_API_KEY as string,
-  appId: process.env.NEXT_PRIVATE_APP_ID as string,
-  authDomain: process.env.NEXT_PRIVATE_AUTH_DOMAIN as string,
-  databaseURL: process.env.NEXT_PRIVATE_DATABASE_URL as string,
-  measurementId: process.env.NEXT_PRIVATE_MEASUREMENT_ID as string,
-  messagingSenderId: process.env.NEXT_PRIVATE_MESSAGING_SENDER_ID as string,
-  projectId: process.env.NEXT_PRIVATE_PROJECT_ID as string,
-  storageBucket: process.env.NEXT_PRIVATE_STORAGE_BUCKET as string,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY as string,
+  appId: process.env.NEXT_PUBLIC_APP_ID as string,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN as string,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL as string,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID as string,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID as string,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID as string,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET as string,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
